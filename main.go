d62f3ed9b5f22a8c2d164ac64de0b409e46453e7
panqinzhang <panqinzhang@ritchiebros.com>
2022-04-07 14:42:24 +0800

    build

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"text/template"
+)
+
+type (
+	Location struct {
+		Street  string
+		ZipCode string
+	}
+
+	User struct {
+		Username  string
+		Locations map[string]Location
+	}
+
+	UsersPage struct {
+		Title string
+		Users []User
+	}
+)
+
+func main() {
+	message, err := ioutil.ReadFile("/Users/panqinzhang/github/fly-go/example.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	t, err := template.New("UsersPage").Parse(string(message))
+	if err != nil {
+		panic(err)
+	}
+
+	p := UsersPage{
+		Title: "测试语法",
+		Users: []User{
+			{
+				Username: "johnsmith",
+				Locations: map[string]Location{
+					"Home": {
+						Street:  "Main Street",
+						ZipCode: "20192",
+					},
+				},
+			},
+		},
+	}
+
+	err = t.Execute(os.Stdout, p)
+	if err != nil {
+		panic(err)
+	}
+}
